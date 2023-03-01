f500db60a350eb576454bb79b3d01378fef3240d
Felix Wieland <mail@felixwie.com>
2023-03-01 21:01:30 +0100

    fix test

--- test/maelstrom/test.sh
+++ test/maelstrom/test.sh
@@ -39,7 +39,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
 
-check "validate favorite color" color | grep 'my favorite color is red'
+check "validate maelstrom" maelstrom | grep 'serve'
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
