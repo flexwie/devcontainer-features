866985063e2f5a247bbc70c779541623f7a409a2
Felix Wieland <mail@felixwie.com>
2023-03-11 19:47:14 +0100

    updated tests

--- src/op/install.sh
+++ src/op/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+apt update && apt install curl -y
+
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
- sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+ gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
-sudo tee /etc/apt/sources.list.d/1password.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | tee /etc/apt/sources.list.d/1password.list
 
-sudo apt update && sudo apt install 1password-cli
+apt update && sudo apt install 1password-cli -y
