6975b368ef27963abbf4aa1124543a4dea2f0fc2
Felix Wieland <mail@felixwie.com>
2023-03-12 10:29:51 +0100

    updated maelstrom

--- src/terraspace/install.sh
+++ src/terraspace/install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+if ! type gem >/dev/null 2>&1; then
+  echo "ERROR: Use a base image with ruby and gem installed, add it to your Dockerfile or use a python feature like https://github.com/devcontainers/features/tree/main/src/ruby"
+  exit 1
+fi
+
+if ! type ruby >/dev/null 2>&1; then
+  echo "ERROR: Use a base image with ruby and gem installed, add it to your Dockerfile or use a python feature like https://github.com/devcontainers/features/tree/main/src/ruby"
+  exit 1
+fi
 
 gem install terraspace
