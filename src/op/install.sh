3345a10b7eed0bc5270ad53326cd397ceb6cf0d8
Felix Wieland <mail@felixwie.com>
2023-03-12 10:21:07 +0100

    updated op

--- src/op/install.sh
+++ src/op/install.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
-apt update && apt install curl -y
+set -e
 
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
- gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+# Clean up
+rm -rf /var/lib/apt/lists/*
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | tee /etc/apt/sources.list.d/1password.list
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
 
-apt update && sudo apt install 1password-cli -y
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Source /etc/os-release to get OS info
+. /etc/os-release
+
+# Install dependencies
+check_packages apt-transport-https curl ca-certificates gnupg2 dirmngr
+# Import key safely (new 'signed-by' method rather than deprecated apt-key approach) and install
+curl -sSL https://downloads.1password.com/linux/keys/1password.asc | gpg --dearmor > /usr/share/keyrings/1password-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" > /etc/apt/sources.list.d/1password.list
+
+# Update lists
+apt-get update -yq
+
+apt-get install -yq 1password-cli  || return 1
